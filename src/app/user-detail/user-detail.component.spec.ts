4cb6029ee4c12089dee74d9df7b75f72c649a460
Muhammad Kamal <mkamalkayani@gmail.com>
2018-06-02 15:33:35 +0200

    Unit test, user-detail component should create is successfull.

--- src/app/user-detail/user-detail.component.spec.ts
+++ src/app/user-detail/user-detail.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailComponent } from './user-detail.component';
+import { UserService } from '../user.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe('UserDetailComponent', () => {
   let component: UserDetailComponent;
   let fixture: ComponentFixture<UserDetailComponent>;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailComponent ]
+      declarations: [ UserDetailComponent ],
+      imports: [ HttpClientModule ],
+      providers: [UserService]
     })
     .compileComponents();
   }));
@@ -16,6 +22,9 @@ describe('UserDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailComponent);
     component = fixture.componentInstance;
+
+    // UserService provided to the TestBed
+    userService = TestBed.get(UserService);
     fixture.detectChanges();
   });
 
