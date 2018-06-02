c0be1e3b77c15285b54ca0a36e89bef3f78d610d
Muhammad Kamal <mkamalkayani@gmail.com>
2018-06-02 05:57:43 +0200

    Unit test of user-list component

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      imports: [
+        HttpClientModule
+      ],
+      providers: [UserService]
     })
     .compileComponents();
   }));
@@ -16,10 +23,17 @@ describe('UserListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+
+    // UserService provided to the TestBed
+    userService = TestBed.get(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have empty array', () => {
+    expect(component.users).toEqual([]);
   });
 });
