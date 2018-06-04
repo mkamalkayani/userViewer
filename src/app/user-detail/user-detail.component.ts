579ed88603f5a71671660338bc18e22441c68ba0
Muhammad Kamal <mkamalkayani@gmail.com>
2018-06-04 23:45:11 +0200

    immplemedted selectedUserAlbums

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { User } from '../user';
+import { AlbumService } from '../album.service';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
-export class UserDetailComponent implements OnInit {
-@Input() public selectedUser: User;
-  constructor() { }
+export class UserDetailComponent implements OnInit, OnChanges {
+  @Input() public selectedUser: User;
+  private albums: Album[];
+  public selectedUserAlbums: Album[];
+
+  constructor(private _albumService: AlbumService) { }
 
   ngOnInit() {
+    this._albumService.getAlbums()
+      .subscribe(data => this.albums = data);
+  }
+
+  filterAlbums(Obj) {
+    return Obj.userId === this.selectedUser.id;
+  }
+
+  ngOnChanges() {
+    if (this.selectedUser) {
+      this.selectedUserAlbums = this.albums.filter(this.filterAlbums, this);
+      console.log(this.selectedUserAlbums);
+    }
   }
 
 }
