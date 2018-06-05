e920382856a0963725e3a4c132122f077f942f0e
Muhammad Kamal <mkamalkayani@gmail.com>
2018-06-05 16:22:05 +0200

    added photo component

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Album } from '../album';
+import { Photo } from '../photo';
+import { PhotoService } from '../photo.service';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit, OnChanges {
+  @Input() public selectedAlbum: Album;
+  private photos: Photo[];
+  public selectedAlbumPhotos: Photo[];
+
+  constructor(private _photoService: PhotoService) { }
+
+  ngOnInit() {
+    this._photoService.getPhotos()
+      .subscribe(data => this.photos = data);
+  }
+
+  filterPhotos(Obj) {
+    return Obj.albumId === this.selectedAlbum.id;
+  }
+
+  ngOnChanges() {
+    if (this.selectedAlbum) {
+      this.selectedAlbumPhotos = this.photos.filter(this.filterPhotos, this);
+      console.log(this.selectedAlbumPhotos);
+    }
+  }
+
+}
