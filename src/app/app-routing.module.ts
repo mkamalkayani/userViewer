af4b14f42ee4d3d85006377b17fed03efce85ecf
Muhammad Kamal <mkamalkayani@gmail.com>
2018-06-12 18:21:49 +0200

    added nav and router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailComponent
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
 })
 export class AppRoutingModule { }
